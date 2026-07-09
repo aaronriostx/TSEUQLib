655df94d67ea644c94fa274bcf35c07cc5365dd0
G. Aaron Rios <gaaronrios@gmail.com>
2026-07-09 11:23:01 -0500

    WIP: making hU a stochastic parameter with imprecise COV

--- examples/FinProblem-Updated/fin_params_mixed.py
+++ examples/FinProblem-Updated/fin_params_mixed.py
@@ -5,14 +5,19 @@ SEED = 2026
 num_of_samples = 50000
 
 # Stochastic parameters (normal distributions)
-COV  = np.array([0.10, 0.03, 0.03, 0.10])
-MEAN = np.array([7.1, 5.800e8, 4.430e-9, 0.114])
+COV  = np.array([0.10, 0.03, 0.03])
+MEAN = np.array([7.1, 5.800e8, 4.430e-9])
 STD  = COV * MEAN
 
 k   = fp.sample("normal", num_of_samples, {"mean": MEAN[0], "std": STD[0]}, seed=SEED)
 Cp  = fp.sample("normal", num_of_samples, {"mean": MEAN[1], "std": STD[1]}, seed=SEED+1)
 rho = fp.sample("normal", num_of_samples, {"mean": MEAN[2], "std": STD[2]}, seed=SEED+2)
-hU  = fp.sample("normal", num_of_samples, {"mean": MEAN[3], "std": STD[3]}, seed=SEED+3)
+
+# Stochastic parameters with epistemic COV
+MEAN_hU = 0.114
+COV_hU = np.array([0.9, 0.11])
+STD_hU  = COV_hU * MEAN_hU
+hU  = fp.sample("normal", num_of_samples, {"mean": MEAN_hU, "std": STD_hU}, seed=SEED+3)
 
 # Epistemic parameters (intervals [lower, upper])
 Tinf = (282.7, 283.3)
